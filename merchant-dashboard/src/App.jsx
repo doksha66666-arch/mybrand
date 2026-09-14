b105bcca6dd675d6741a4a922d2880c35a4f39dd
doksha66666-arch <doksha66666@gmail.com>
2026-09-14 12:01:30 +0300

    Separate Merchant Portal from embedded Admin routes

--- merchant-dashboard/src/App.jsx
+++ merchant-dashboard/src/App.jsx
@@ -6,7 +6,6 @@ import Layout from './components/Layout';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import UnifiedLoginPage from './UnifiedLoginPage';
-import AdminEmbeddedApp from './AdminEmbeddedApp';
 import DashboardPage from './pages/DashboardPage';
 import ProductsPage from './pages/ProductsPage';
 import MerchantFulfillmentCenterPage from './pages/MerchantFulfillmentCenterPage';
@@ -24,7 +23,6 @@ export default function App() {
           <Route path="/login" element={<UnifiedLoginPage />} />
           <Route path="/merchant-login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
-          <Route path="/admin/*" element={<AdminEmbeddedApp />} />
           <Route path="/" element={<ProtectedRoute><Layout /></ProtectedRoute>}>
             <Route index element={<DashboardPage />} />
             <Route path="studio" element={<StudioPage />} />
