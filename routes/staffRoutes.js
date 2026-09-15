6cde7929abbced4995670749a5503bb5027c92c4
doksha66666-arch <doksha66666@gmail.com>
2026-09-15 16:57:33 +0300

    Expose staff account management endpoints

--- routes/staffRoutes.js
+++ routes/staffRoutes.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
-const { listStaff, createStaff } = require('../controllers/staffController');
+const { listStaff, createStaff, updateStaff, removeStaff } = require('../controllers/staffController');
 const { protect, adminOnly } = require('../middleware/auth');
 
 router.get('/', protect, adminOnly, listStaff);
 router.post('/', protect, adminOnly, createStaff);
+router.patch('/:id', protect, adminOnly, updateStaff);
+router.delete('/:id', protect, adminOnly, removeStaff);
 
 module.exports = router;
