bdde2a305527e306875495e24d9c22f26bc9ed40
doksha66666-arch <doksha66666@gmail.com>
2026-09-18 11:39:26 +0400

    perf: lighten support ticket inbox (#20)
    * perf: lighten support ticket inbox
    
    * feat: add admin support ticket detail route
    
    * perf: fetch support ticket details on selection
    
    * fix: normalize support controller line breaks
    
    * fix: normalize support route line breaks

--- routes/supportRoutes.js
+++ routes/supportRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { protect, adminOnly } = require('../middleware/auth');
-const { getSupportConfig, updateSupportConfig, createTicket, myTickets, allTickets, replyTicket, adminUpdateTicket } = require('../controllers/supportController');
+const { getSupportConfig, updateSupportConfig, createTicket, myTickets, allTickets, getAdminTicket, replyTicket, adminUpdateTicket } = require('../controllers/supportController');
 
 router.get('/', getSupportConfig);
 router.put('/', protect, adminOnly, updateSupportConfig);
@@ -9,6 +9,7 @@ router.post('/tickets', protect, createTicket);
 router.get('/tickets/my', protect, myTickets);
 router.post('/tickets/:id/messages', protect, replyTicket);
 router.get('/tickets/all', protect, adminOnly, allTickets);
+router.get('/tickets/:id', protect, adminOnly, getAdminTicket);
 router.put('/tickets/:id', protect, adminOnly, adminUpdateTicket);
 
 module.exports = router;
