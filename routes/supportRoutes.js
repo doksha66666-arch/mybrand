29c0ae2951dc2184ea5628cf32d503f68de9d92b
doksha66666-arch <doksha66666@gmail.com>
2026-09-18 11:38:24 +0400

    fix: normalize support route line breaks

--- routes/supportRoutes.js
+++ routes/supportRoutes.js
@@ -8,7 +8,8 @@ router.put('/', protect, adminOnly, updateSupportConfig);
 router.post('/tickets', protect, createTicket);
 router.get('/tickets/my', protect, myTickets);
 router.post('/tickets/:id/messages', protect, replyTicket);
-router.get('/tickets/all', protect, adminOnly, allTickets);\nrouter.get('/tickets/:id', protect, adminOnly, getAdminTicket);
+router.get('/tickets/all', protect, adminOnly, allTickets);
+router.get('/tickets/:id', protect, adminOnly, getAdminTicket);
 router.put('/tickets/:id', protect, adminOnly, adminUpdateTicket);
 
 module.exports = router;
